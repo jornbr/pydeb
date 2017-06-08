10c0ffad82f9f6862b17099c1c64a4270432e20c
Jorn Bruggeman <jbr@pml.ac.uk>
2017-06-08 16:23:52 +0100

    add labels when plotting distributions

--- plot_distribution.py
+++ plot_distribution.py
@@ -1,15 +1,27 @@
 import io
 import numpy
 from matplotlib import pyplot
+import argparse
+
+path = 'traits.txt'
+parser = argparse.ArgumentParser()
+parser.add_argument('path')
+args = parser.parse_args()
 
 trait2data = {}
-with io.open('traits.txt', 'rU', encoding='utf-8') as f:
+trait2label = {}
+with io.open(args.path, 'rU', encoding='utf-8') as f:
     labels = f.readline().rstrip('\n').split('\t')
+    traits = []
+    for label in labels[2:]:
+        trait = label.split(' (', 1)[0]
+        trait2label[trait] = label
+        traits.append(trait)
     for l in f:
         items = l.rstrip('\n').split('\t')
-        for trait, value in zip(labels[2:], items[2:]):
+        for trait, value in zip(traits, items[2:]):
             if value != '':
-                trait2data.setdefault(trait.split(' (')[0], []).append(float(value))
+                trait2data.setdefault(trait, []).append(float(value))
 
 fig = pyplot.figure(figsize=(15, 6))
 ax_lin = fig.add_subplot(121)
@@ -20,6 +32,7 @@ for trait, values in trait2data.items():
     ax_lin.hist(values, bins=max(len(values)/5, 10))
     ax_lin.set_title('%s (linear axis)' % trait)
     ax_lin.grid()
+    ax_lin.set_xlabel(trait2label[trait])
     logvalues = numpy.log10(values[values>0])
     ax_log.cla()
     ax_log.set_title('%s (log axis) - %i values <= 0 ignored' % (trait, len(values)-len(logvalues)))
@@ -28,6 +41,7 @@ for trait, values in trait2data.items():
         ax_log.hist(values, bins=10.**logrange)
         ax_log.set_xscale('log')
         ax_log.grid()
+        ax_log.set_xlabel(trait2label[trait])
     print 'saving %s...' % trait
     fig.savefig('distribution %s.png' % trait, dpi=150)
 
