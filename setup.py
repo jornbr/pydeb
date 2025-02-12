522e32be0be4b43e13e8d06f6b3f6ab5afa2b290
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-02-12 17:31:36 +0000

    clean-up setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
 import numpy
 
 setup(
-      ext_modules = [
-          Extension('pydeb.engine.cmodel', ['pydeb/engine/cmodel.pyx']),
-          Extension('pydeb.engine.optimize', ['pydeb/engine/optimize.pyx'])
-      ],
-      include_dirs=[numpy.get_include()],
+    ext_modules=[
+        Extension('pydeb.engine.cmodel', ['pydeb/engine/cmodel.pyx']),
+        Extension('pydeb.engine.optimize', ['pydeb/engine/optimize.pyx'])
+    ],
+    include_dirs=[numpy.get_include()],
 )
